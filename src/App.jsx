0b3610ec62645a451c2917b541c71f3a626f0f85
Qasimm-Ali <qqali776655@gmail.com>
2024-06-24 15:44:36 +0500

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Input from "./Components/Input";
+
+export default function App(){
+  return(
+    <>   
+    <div className="flex justify-center items-center h-screen bg-slate-400 ">
+      <Input/>
+    </div>
+    </>
+  )
+}
